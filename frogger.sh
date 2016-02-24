300afca3cddc0bd005dcfc4a90234ce3d4654e99
Felix Ingram <lllama@users.noreply.github.com>
2016-02-24 09:53:38 +0000

    Change VLANIDS to use the new TS_FILTER_FLAG

--- frogger.sh
+++ frogger.sh
@@ -333,7 +333,7 @@ echo ""
 echo -e "\e[01;32m[-]\e[00m Now Extracting VLAN IDs on interface $INT, sniffing 802.1Q tagged packets for "$TAGSEC" seconds."
 echo ""
 
-VLANIDS=$(tshark -a duration:$TAGSEC -i $INT -R "vlan" -x -V 2>&1 |grep -o " = ID: .*" |awk '{ print $NF }' | sort --unique)
+VLANIDS=$(tshark -a duration:$TAGSEC -i $INT $TS_FILTER_FLAG "vlan" -x -V 2>&1 |grep -o " = ID: .*" |awk '{ print $NF }' | sort --unique)
 
 if [ -z "$VLANIDS" ]
 	then
